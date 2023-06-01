5ce723b3f8a6bf81253fd89c218b68e231b755f3
Aymeric Beauchamp <aymeric.beauchamp@etu.univ-orleans.fr>
2023-06-01 15:14:36 +0200

    Changing workflow

--- test_query.py
+++ test_query.py
@@ -7,15 +7,17 @@ from active_semi_clustering.semi_supervised.pairwise_constraints import COPKMean
 from sklearn.decomposition import PCA
 from sklearn.metrics import adjusted_rand_score
 
-# Choose a dataset from https://clustering-benchmarks.gagolewski.com/weave/data-v1.html
-dataset = clustbench.load_dataset("fcps", "lsun", path="clustering-data-v1")
-labels = dataset.labels[0] - 1 # correspondance between clustbench and Python indexing
 
-algo = COPKMeans(n_clusters=dataset.n_clusters[0])
-algo.fit(dataset.data)
-init_partition = algo.labels_
+def test_query():
+    # Choose a dataset from https://clustering-benchmarks.gagolewski.com/weave/data-v1.html
+    dataset = clustbench.load_dataset("fcps", "lsun", path="clustering-data-v1")
+    labels = dataset.labels[0] - 1  # correspondance between clustbench and Python indexing
 
-qs = AIPC(dataset.n_clusters[0])
+    algo = COPKMeans(n_clusters=dataset.n_clusters[0])
+    algo.fit(dataset.data)
+    init_partition = algo.labels_
 
-constraints = qs.fit(dataset.data, algo.labels_, MLCLOracle(truth=labels, budget=100))
-algo.fit(dataset.data, ml=constraints["ml"], cl=constraints["cl"])
+    qs = AIPC(dataset.n_clusters[0])
+
+    constraints = qs.fit(dataset.data, algo.labels_, MLCLOracle(truth=labels, budget=100))
+    algo.fit(dataset.data, ml=constraints["ml"], cl=constraints["cl"])
