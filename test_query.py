52db7b4976394cac57a8c2f8b687be4fd0986f7c
Aymeric Beauchamp <aymeric.beauchamp@etu.univ-orleans.fr>
2023-06-01 15:27:54 +0200

    Add random sampling and NPU to tests

--- test_query.py
+++ test_query.py
@@ -1,11 +1,7 @@
-import pandas as pd
-import argparse
-import clustbench
-from skquery import AIPC
+himport clustbench
+from skquery import *
 from skquery.oracle import MLCLOracle
-from active_semi_clustering.semi_supervised.pairwise_constraints import COPKMeans, PCKMeans, MPCKMeans
-from sklearn.decomposition import PCA
-from sklearn.metrics import adjusted_rand_score
+from active_semi_clustering.semi_supervised.pairwise_constraints import COPKMeans
 
 
 def test_query():
@@ -15,9 +11,9 @@ def test_query():
 
     algo = COPKMeans(n_clusters=dataset.n_clusters[0])
     algo.fit(dataset.data)
-    init_partition = algo.labels_
 
-    qs = AIPC(dataset.n_clusters[0])
+    for strat in [Random, NPUincr, AIPC]:
+        qs = strat(dataset.n_clusters[0])
 
-    constraints = qs.fit(dataset.data, algo.labels_, MLCLOracle(truth=labels, budget=100))
-    algo.fit(dataset.data, ml=constraints["ml"], cl=constraints["cl"])
+        constraints = qs.fit(dataset.data, algo.labels_, MLCLOracle(truth=labels, budget=100))
+        algo.fit(dataset.data, ml=constraints["ml"], cl=constraints["cl"])
