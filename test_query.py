64bdd727f907e6d8798fbe0c84019c60f2780925
Aymeric Beauchamp <aymeric.beauchamp@etu.univ-orleans.fr>
2023-06-02 11:28:35 +0200

    Simplified test

--- test_query.py
+++ test_query.py
@@ -1,8 +1,7 @@
 import clustbench
 from skquery import *
 from skquery.oracle import MLCLOracle
-from active_semi_clustering.semi_supervised.pairwise_constraints import COPKMeans
-from time import time
+from sklearn.cluster import KMeans
 
 
 def test_query():
@@ -10,14 +9,9 @@ def test_query():
     dataset = clustbench.load_dataset("fcps", "lsun", path="clustering-data-v1")
     labels = dataset.labels[0] - 1  # correspondance between clustbench and Python indexing
 
-    algo = COPKMeans(n_clusters=dataset.n_clusters[0])
+    algo = KMeans(n_clusters=dataset.n_clusters[0])
     algo.fit(dataset.data)
 
-    for strat in [Random, FFQS, MinMax, NPUincr, AIPC]:
+    for strat in [Random, FFQS, MinMax, NPUincr, AIPC, SASC]:
         qs = strat(dataset.n_clusters[0])
-
-        t1 = time()
-        constraints = qs.fit(dataset.data, algo.labels_, MLCLOracle(truth=labels, budget=100))
-        print(f"{strat.__name__} : {time() - t1} seconds to fit")
-
-        algo.fit(dataset.data, ml=constraints["ml"], cl=constraints["cl"])
+        qs.fit(dataset.data, algo.labels_, MLCLOracle(truth=labels, budget=10))
