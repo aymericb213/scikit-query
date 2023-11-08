7eeb7f264c793951ad3afa0b3be7d4dc285a07da
Aymeric Beauchamp <aymeric.beauchamp@etu.univ-orleans.fr>
2023-11-08 16:55:51 +0100

    Fix theme

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,7 @@ autodoc_default_flags = ['members', 'special-members']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "alabaster"
 html_static_path = ['_static']
 
 # Output file base name for HTML help builder.
